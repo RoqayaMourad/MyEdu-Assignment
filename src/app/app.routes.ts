ee97c5871d7a62bd895eae42ecd54bb6f15235e0
roqayamourad <roqayamourad@gmail.com>
2024-12-14 00:47:24 +0200

    footer and footer responsiveness , routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+  { path: 'subscribe', component: SubscribeComponent },
   {
     path: '',
     redirectTo: 'home',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
+  safelist: [
+    'peer',
+    'peer-checked',
+    'fill-purple-700',
+  ],
   theme: {
     screens: {
       'sm' : '640px',
