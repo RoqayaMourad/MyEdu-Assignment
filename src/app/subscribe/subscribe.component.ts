3cf7a36386e100068fe58f10483ed729a9b0f54e
roqayamourad <roqayamourad@gmail.com>
2024-12-14 00:57:24 +0200

    footer for subscription page

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrls: ['./subscribe.component.scss'],
   standalone: true,
+  imports: [ IonContent, HeaderComponent, FormsModule, ReactiveFormsModule, CommonModule ],
 })
 export class SubscribeComponent  implements OnInit {
 
