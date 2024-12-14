7e4385ec3fb85e510d9290d77471586027eeafc9
roqayamourad <roqayamourad@gmail.com>
2024-12-14 12:06:03 +0200

    testing form validity

--- src/app/subscribe/subscribe.component.spec.ts
+++ src/app/subscribe/subscribe.component.spec.ts
@@ -32,4 +32,16 @@ describe('SubscribeComponent', () => {
     component.subscriptionForm.controls['email'].setValue('');
     expect(component.subscriptionForm.valid).toBeFalse();
   });
+  it('should validate the form if all required fields are filled', () => {
+    component.subscriptionForm.controls['name'].setValue('Test Name');
+    component.subscriptionForm.controls['email'].setValue('test@example.com');
+    component.subscriptionForm.controls['childName'].setValue('Child Name');
+    component.subscriptionForm.controls['birthDay'].setValue(1);
+    component.subscriptionForm.controls['birthMonth'].setValue('January');
+    component.subscriptionForm.controls['birthYear'].setValue(2020);
+    component.subscriptionForm.controls['grade'].setValue('Grade 2');
+    component.subscriptionForm.controls['gender'].setValue('Boy');
+  
+    expect(component.subscriptionForm.valid).toBeTrue();
+  });
 });
