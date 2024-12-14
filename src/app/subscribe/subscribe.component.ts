a8494bca90301e0e83bccc8381658ede6f0acabc
roqayamourad <roqayamourad@gmail.com>
2024-12-14 11:38:27 +0200

    form header

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -4,17 +4,23 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrls: ['./subscribe.component.scss'],
   standalone: true,
-  imports: [ IonContent, HeaderComponent, FormsModule, ReactiveFormsModule, CommonModule ],
+  imports: [ IonContent, HeaderComponent, FormsModule,
+    ReactiveFormsModule, CommonModule],
 })
 export class SubscribeComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
 
+  }
+
+
+  
   ngOnInit() {}
 
 }
